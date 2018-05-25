079e6973e89dbc171dbe67a3af24c117ffa3bc94
branchard <benoit.branchard@gmail.com>
2018-05-25 23:40:28 +0200

    finish the leaderboard

--- src/components/views/Leaderboard.vue
+++ src/components/views/Leaderboard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <md-table v-model="searched" md-sort="score" md-sort-order="desc" md-card md-fixed-header>
+    <div class="leaderboard">
+        <md-table v-model="searched" md-sort="rank" md-sort-order="desc" md-card md-fixed-header>
             <md-table-toolbar>
                 <div class="md-toolbar-section-start">
-                    <h1 class="md-title">Leaderboard</h1>
+                    <h1 class="md-title">Leaderboard (Top 100)</h1>
                 </div>
                 <md-field md-clearable class="md-toolbar-section-end">
                     <md-input placeholder="Search by nickname..." v-model="search" @input="searchOnTable" />
@@ -11,21 +11,32 @@
             </md-table-toolbar>
 
             <md-table-empty-state
-                md-label="Leaderboard is empty"
-                md-description="There is no users in leaderboard">
+                md-label="Leaderboard is loading..."
+                md-description="Please wait...">
             </md-table-empty-state>
 
             <md-table-row slot="md-table-row" slot-scope="{ item }">
-                <md-table-cell md-label="Rank" md-sort-by="id" md-numeric>{{ 1 }}</md-table-cell>
-                <md-table-cell md-label="Nickname" md-sort-by="name">{{ item.nickname }}</md-table-cell>
-                <md-table-cell md-label="Score" md-sort-by="email">{{ item.score }}</md-table-cell>
-                <md-table-cell md-label="Time" md-sort-by="gender">{{ item.time }}</md-table-cell>
+                <md-table-cell md-label="Rank" md-sort-by="rank"># {{ item.rank }}</md-table-cell>
+                <md-table-cell md-label="Nickname" md-sort-by="nickname">{{ item.nickname }}</md-table-cell>
+                <md-table-cell md-label="Score" md-sort-by="score" md-numeric>{{ item.score }}</md-table-cell>
+                <md-table-cell md-label="Time" md-sort-by="time" md-numeric>{{ Number.parseFloat(item.time).toFixed(2) }} sec</md-table-cell>
             </md-table-row>
         </md-table>
     </div>
 </template>
 
 <style lang="scss" scoped>
+    .leaderboard {
+        width: 100%;
+        height: 100%;
+
+        .md-table {
+            width: 100%;
+            height: 100%;
+            margin-right: 0;
+            margin-left: 0;
+        }
+    }
     .md-field {
         max-width: 300px;
     }
--- src/utils/leaderboardApi.js
+++ src/utils/leaderboardApi.js
@@ -6,8 +6,34 @@ export default  {
         fetch(`https://leaderboard.lp1.eu/api/${isBot ? botNickname : humanNickname}/${score}/${time}`);
     },
     getLeaderbord: function(){
-        return fetch('https://leaderboard.lp1.eu/api/json').then(function(response) {
-            return response.json();
-        });
+        return new Promise((resolve, reject) => {
+            fetch('https://leaderboard.lp1.eu/api/json').then(function(response) {
+                return response.json();
+            }).then((leaderboard) => {
+                // filter leaderboard
+                let filteredLeaderboard = leaderboard.filter(entry => {
+                    return entry.nickname &&
+                        entry.nickname != '' && 
+                        entry.score &&
+                        entry.score != '' &&
+                        !isNaN(entry.score) &&
+                        entry.time &&
+                        entry.time != '' &&
+                        !isNaN(entry.time);
+                });
+
+                let sortedLeaderboard = filteredLeaderboard.sort((a, b) => {
+                    return b.score - a.score;
+                });
+
+                // keep only 100 first
+                let leaderboardWithRanks = sortedLeaderboard.slice(0, 100).map((entry, index) => {
+                    entry.rank = index + 1;
+                    return entry;
+                });
+
+                resolve(leaderboardWithRanks);
+            });
+        })
     },
 };

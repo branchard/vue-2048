91b3016f760fda4e740e6d703eb0027842736a27
branchard <benoit.branchard@gmail.com>
2018-05-25 16:55:31 +0200

    add leaderboard

--- src/components/views/Leaderboard.vue
+++ src/components/views/Leaderboard.vue
@@ -1,11 +1,68 @@
 <template>
     <div>
-        Leaderboard
+        <md-table v-model="searched" md-sort="score" md-sort-order="desc" md-card md-fixed-header>
+            <md-table-toolbar>
+                <div class="md-toolbar-section-start">
+                    <h1 class="md-title">Leaderboard</h1>
+                </div>
+                <md-field md-clearable class="md-toolbar-section-end">
+                    <md-input placeholder="Search by nickname..." v-model="search" @input="searchOnTable" />
+                </md-field>
+            </md-table-toolbar>
+
+            <md-table-empty-state
+                md-label="Leaderboard is empty"
+                md-description="There is no users in leaderboard">
+            </md-table-empty-state>
+
+            <md-table-row slot="md-table-row" slot-scope="{ item }">
+                <md-table-cell md-label="Rank" md-sort-by="id" md-numeric>{{ 1 }}</md-table-cell>
+                <md-table-cell md-label="Nickname" md-sort-by="name">{{ item.nickname }}</md-table-cell>
+                <md-table-cell md-label="Score" md-sort-by="email">{{ item.score }}</md-table-cell>
+                <md-table-cell md-label="Time" md-sort-by="gender">{{ item.time }}</md-table-cell>
+            </md-table-row>
+        </md-table>
     </div>
 </template>
 
+<style lang="scss" scoped>
+    .md-field {
+        max-width: 300px;
+    }
+</style>
+
 <script>
+import leaderboardApi from '@/utils/leaderboardApi';
+
+const toLower = text => {
+    return text.toString().toLowerCase();
+}
+
+const searchByName = (items, term) => {
+    if (term) {
+        return items.filter(item => toLower(item.nickname).includes(toLower(term)));
+    }
+
+    return items;
+}
+
 export default {
-    name: 'Leaderboard'
+    name: "Leaderboard",
+    data: () => ({
+        search: null,
+        searched: [],
+        leaderboard: []
+    }),
+    methods: {
+        searchOnTable () {
+            this.searched = searchByName(this.leaderboard, this.search)
+        }
+    },
+    created: function () {
+        leaderboardApi.getLeaderbord().then((data) => {
+            this.leaderboard = data;
+            this.searched = data;
+        });
+    }
 };
 </script>
--- src/utils/leaderboardApi.js
+++ src/utils/leaderboardApi.js
@@ -4,5 +4,10 @@ const humanNickname = 'John Cena';
 export default  {
     postScore: function(score, time, isBot){
         fetch(`https://leaderboard.lp1.eu/api/${isBot ? botNickname : humanNickname}/${score}/${time}`);
-    }
+    },
+    getLeaderbord: function(){
+        return fetch('https://leaderboard.lp1.eu/api/json').then(function(response) {
+            return response.json();
+        });
+    },
 };
